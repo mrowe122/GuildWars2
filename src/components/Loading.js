cbff45fbed27e921368b6401ed6f6e27f098100d
mrowe122 <mrowe122@gmail.com>
2018-05-16 15:46:35 -0400

    created a full page loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import Spinner from 'react-spinkit'
 
 const LoadingTemplate = ({ className }) => (
   <div className={className} />
@@ -10,6 +11,29 @@ LoadingTemplate.propTypes = {
   className: PropTypes.string
 }
 
-const Loading = styled(LoadingTemplate)``
+export const Loading = styled(LoadingTemplate)``
 
-export default Loading
+const FullPageLoaderTemplate = ({ className }) => (
+  <div className={className}>
+    <Spinner name='three-bounce' fadeIn='none' />
+  </div>
+)
+
+FullPageLoaderTemplate.propTypes = {
+  className: PropTypes.string
+}
+
+export const FullPageLoader = styled(FullPageLoaderTemplate)`
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: ${({ theme }) => theme.zIndexLayers.modalOverlay};
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.loadingOverlay};
+
+  .sk-spinner { color: ${({ theme }) => theme.colors.primaryLight1}; }
+`
