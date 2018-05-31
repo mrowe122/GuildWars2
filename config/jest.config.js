0eb65a11c82ab7d3618710153b2097c241840d17
mrowe122 <mrowe122@gmail.com>
2018-05-30 21:40:04 -0400

    configuring testing framework

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  rootDir: '../src',
+  rootDir: '../',
+  setupFiles: ['./tests/jest.setup.js'],
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.js'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.js'],
+  coverageReporters: ['text', 'lcov'],
   coverageThreshold: {
     global: {
       branches: 80,
@@ -9,5 +12,9 @@ module.exports = {
       lines: 80,
       statements: 80
     }
+  },
+  'moduleNameMapper': {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'tests/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy'
   }
 }
