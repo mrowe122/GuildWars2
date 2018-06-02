40054897a424d4d8ff1211c78f54bf4a8eb96f65
mrowe122 <mrowe122@gmail.com>
2018-06-02 01:33:02 -0400

    configuration clean up and updated dependencies

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Enzyme, { shallow, render, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import theme from '../src/styles/variables'
@@ -7,3 +8,5 @@ global.shallow = shallow
 global.render = render
 global.mount = mount
 global.globalProps = { theme }
+global.fetch = require('jest-fetch-mock')
+global.EmptyDiv = () => <div />
