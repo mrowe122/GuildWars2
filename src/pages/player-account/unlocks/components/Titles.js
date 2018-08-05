13b03ca490b9b5bf690533f66b6499ab9b4d980d
mrowe122 <mrowe122@gmail.com>
2018-08-04 22:53:14 -0400

    updated titles and updated mock server

--- src/pages/player-account/unlocks/components/Titles.js
+++ src/pages/player-account/unlocks/components/Titles.js
@@ -1,5 +1,6 @@
 /* istanbul ignore file */
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'react-emotion'
 import { compose, get } from 'lodash/fp'
 import { withConsumer } from 'context-hoc'
@@ -11,15 +12,28 @@ const SkinGroup = styled.div`
   text-align: left;
   color: ${({ theme }) => theme.colors.white};
   margin-bottom: 2rem;
+
+  h3 {
+    color: ${({ theme }) => theme.colors.primaryLight1};
+  }
 `
 
-const Titles = ({ titles }) =>
-  titles.map(t => (
-    <SkinGroup key={t.id}>
-      <h3>{t.name}</h3>
-      <p>{cleanString(get('achievements[0].requirement')(t)) || cleanString(get('achievements[0].description')(t))}</p>
-    </SkinGroup>
-  ))
+const Titles = ({ titles }) => (
+  <div className="row">
+    {titles.map(t => (
+      <SkinGroup key={t.id} className="col-xs-6">
+        <h3>{t.name}</h3>
+        <p>
+          {cleanString(get('achievements[0].requirement')(t)) || cleanString(get('achievements[0].description')(t))}
+        </p>
+      </SkinGroup>
+    ))}
+  </div>
+)
+
+Titles.propTypes = {
+  titles: PropTypes.array
+}
 
 const TitlesEnhancer = compose(
   withConsumer('app'),
